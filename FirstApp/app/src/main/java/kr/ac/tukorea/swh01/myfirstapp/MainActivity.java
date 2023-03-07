d3f9e4b90021bc17f8362d6bb364d72e6e4293ee
Woo Hyuk Sung <tjddngur123@gmail.com>
2023-03-07 16:27:04 +0900

    FirstApp 생성 후 커밋

---
+++ FirstApp/app/src/main/java/kr/ac/tukorea/swh01/myfirstapp/MainActivity.java
@@ -0,0 +1,14 @@
+package kr.ac.tukorea.swh01.myfirstapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
