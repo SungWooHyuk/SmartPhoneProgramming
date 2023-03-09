d61bf57c9db8825679fd34c0679b0461c202ab5c
Woo Hyuk Sung <tjddngur123@gmail.com>
2023-03-09 15:30:17 +0900

    즉석에서 객체 만들어서 버튼 넘기기

--- FirstApp/app/src/main/java/kr/ac/tukorea/swh01/myfirstapp/MainActivity.java
+++ FirstApp/app/src/main/java/kr/ac/tukorea/swh01/myfirstapp/MainActivity.java
@@ -7,7 +7,7 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+public class MainActivity extends AppCompatActivity{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,19 +15,12 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         setContentView(R.layout.activity_main);
 
         Button btn = findViewById(R.id.PushMeButton);
-        btn.setOnClickListener(this); // 나한테 알려주기, on clickedListener여야함.  interface를 지켜주기만하면됌
-       // 이 this가 아래 onCLick을 가르킴
-    }
-    // MainActivity가 화면을 구성하고 버튼을 누르면 클릭을 감지하니까 onclick으로가서 text를 바꿔준다.
-    // 인터페이스를 implements
-
-    @Override
-    public void onClick(View view) {
-        switch (view.getId()) {
-            case R.id.PushMeButton:
-                break;
-        }
-        TextView tv = findViewById(R.id.sNumTextView);
-        tv.setText("PushMe button clicked");
+        btn.setOnClickListener(new View.OnClickListener() { // 즉석에서 객체 하나 만들어서 던져줌
+            @Override
+            public void onClick(View view) {
+                TextView tv = findViewById(R.id.sNumTextView);
+                tv.setText("PushMe");
+            }
+        });
     }
 }
